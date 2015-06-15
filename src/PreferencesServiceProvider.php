e9b18f53b420ddd0150aa03e5634f4adb3b194bf
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-15 20:29:04 +1000

    Stripped package of all interface functionality, moved to `oxygen/mod-preferences`

--- src/PreferencesServiceProvider.php
+++ src/PreferencesServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oxygen\Preferences;
+namespace OxygenModule\Preferences;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -24,25 +24,7 @@ class PreferencesServiceProvider extends ServiceProvider {
 	 */
 
 	public function boot() {
-		$this->package('oxygen/preferences', 'oxygen/preferences', __DIR__ . '/../resources');
-
-        $this->app['oxygen.blueprintManager']->loadDirectory(__DIR__ . '/../resources/blueprints');
         $this->app['oxygen.preferences']->loadDirectory(__DIR__ . '/../resources/preferences');
-
-        $this->addNavigationItems();
-	}
-
-	/**
-	 * Adds items the the admin navigation.
-	 *
-	 * @return void
-	 */
-
-	public function addNavigationItems() {
-		$blueprint = $this->app['oxygen.blueprintManager']->get('Preferences');
-		$nav = $this->app['oxygen.navigation'];
-
-		$nav->add($blueprint->getToolbarItem('getView'));
 	}
 
 	/**
@@ -52,7 +34,7 @@ class PreferencesServiceProvider extends ServiceProvider {
 	 */
 
 	public function register() {
-	    $this->app->bindShared(['oxygen.preferences' => 'Oxygen\Preferences\PreferencesManager'], function() {
+	    $this->app->singleton(PreferencesManager::class, function() {
 	        return new PreferencesManager();
 	    });
 	}
@@ -65,7 +47,7 @@ class PreferencesServiceProvider extends ServiceProvider {
 
 	public function provides() {
 		return [
-			'oxygen.preferences'
+            PreferencesManager::class
 		];
 	}
 
